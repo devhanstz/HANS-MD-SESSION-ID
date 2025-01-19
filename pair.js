8dfdf1e78e9c5121ec16c6378ba52f533671c934
devhanstz <hanstechmd@gmail.com>
2025-01-19 05:50:59 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -54,7 +54,7 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'HANSTZ-MD;;;' + b64data });
+               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'HANSTZ-TECH;;;' + b64data });
 
                let SIGMA_MD_TEXT = `
 *FELIX-XMD IS CONNECTED ENJOY*
